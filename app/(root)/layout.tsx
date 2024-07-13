4395014fcb627ba8084180f30e78b9a0ebdca7e9
Ahlemmer <ahlemmerabtene@gmail.com>
2024-07-14 00:06:32 +0200

    feat/create-sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 
+import SideBar from "@/components/sideBar";
 
 export default function RootLayout({
   children,
@@ -6,7 +7,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="flex h-screen w-full font-inter">
+          <SideBar user={{
+            $id:'1',
+            address1:'gg',
+            city:'hhh',
+            dateOfBirth:'hhh',
+            dwollaCustomerId:'gg',
+            dwollaCustomerUrl:'hhh',
+            email:'nnnn',
+            firstName:'ggg',
+            lastName:'hh',
+            postalCode:':1',
+            ssn:'55',
+            state:'ff',
+            userId:'gg'
+
+          }}/>
          
           {children}
     </main>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+  transpilePackages: ['@acme/ui', 'lodash-es'],
+  experimental: {
+    swcTraceProfiling: true,
+  },
+  compiler: {
+    removeConsole: true,
+  },
+};
 
 export default nextConfig;
