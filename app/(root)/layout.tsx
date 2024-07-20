3d46babbdf0dd96616b327af399b9feb912d9b1b
Ahlemmer <ahlemmerabtene@gmail.com>
2024-07-20 16:42:00 +0200

    create right-side-bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
-
-import SideBar from "@/components/sideBar";
+import SideBar from '@/components/sideBar';
+import MobileNav from '@/components/mobile-nav-bar';
+import Image from 'next/image';
+import RightSidebar from '@/components/right-side-bar';
 
 export default function RootLayout({
   children,
@@ -8,24 +10,68 @@ export default function RootLayout({
 }>) {
   return (
     <main className="flex h-screen w-full font-inter">
-          <SideBar user={{
-            $id:'1',
-            address1:'gg',
-            city:'hhh',
-            dateOfBirth:'hhh',
-            dwollaCustomerId:'gg',
-            dwollaCustomerUrl:'hhh',
-            email:'nnnn',
-            firstName:'ggg',
-            lastName:'hh',
-            postalCode:':1',
-            ssn:'55',
-            state:'ff',
-            userId:'gg'
+      <SideBar
+        user={{
+          $id: '1',
+          address1: 'gg',
+          city: 'hhh',
+          dateOfBirth: 'hhh',
+          dwollaCustomerId: 'gg',
+          dwollaCustomerUrl: 'hhh',
+          email: 'nnnn',
+          firstName: 'ggg',
+          lastName: 'hh',
+          postalCode: ':1',
+          ssn: '55',
+          state: 'ff',
+          userId: 'gg',
+        }}
+      />
 
-          }}/>
-         
-          {children}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav
+              user={{
+                $id: '1',
+                address1: 'gg',
+                city: 'hhh',
+                dateOfBirth: 'hhh',
+                dwollaCustomerId: 'gg',
+                dwollaCustomerUrl: 'hhh',
+                email: 'nnnn',
+                firstName: 'ggg',
+                lastName: 'hh',
+                postalCode: ':1',
+                ssn: '55',
+                state: 'ff',
+                userId: 'gg',
+              }}
+            />
+          </div>
+        </div>
+        {children}
+      </div>
+      <RightSidebar
+        banks={[{currentBalance:12.34},{currentBalance:123.7}]}
+        transactions={[]}
+        user={{
+          $id: '1',
+          address1: 'gg',
+          city: 'hhh',
+          dateOfBirth: 'hhh',
+          dwollaCustomerId: 'gg',
+          dwollaCustomerUrl: 'hhh',
+          email: 'nnnn',
+          firstName: 'ggg',
+          lastName: 'hh',
+          postalCode: ':1',
+          ssn: '55',
+          state: 'ff',
+          userId: 'gg',
+        }}
+      />
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,19 +7,24 @@ function Home() {
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox
-            type={'greeting'}
-            title="Welcome"
-            user={'ahlem'}
-            subtext={'Access and manage your account efficiently'}
+          <HeaderBox 
+             type={'greeting'}
+             title="Welcome"
+             user={'ahlem'}
+             subtext={'Access and manage your account efficiently'}
           />
-          <TotalBalanceBox
+
+          <TotalBalanceBox 
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1288.9}
           />
         </header>
+
+     
       </div>
+
+    
     </section>
   );
 }
